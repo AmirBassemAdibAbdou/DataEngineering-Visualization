8309d653354e47f764b397c9be16062056f6ce6c
Amir Bassem <amirbassememail@gmail.com>
2025-11-21 22:06:01 +0200

    Update index.py

--- Dashboard/api/index.py
+++ Dashboard/api/index.py
@@ -1,4 +1,8 @@
-from app import app
-from vercel_python_utils import VercelHandler
+import os
+import sys
 
-handler = VercelHandler(app.server)
+# Add the parent directory to the system path to find app.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import the server from your app.py and rename it to 'app' for Vercel
+from app import server as app
